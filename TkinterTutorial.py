8f822fe442fdb68cbeb4f8e2e0b910081c5db298
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-26 22:09:11 -0600

    Add Tkinter Tutorial save

--- TkinterTutorial.py
+++ TkinterTutorial.py
@@ -5,7 +5,7 @@ root = Tk()
 
 # Creating a label widget
 myLabel = Label(root, text="Hello World!")
-# Shoving it onto the screen
+# Shoving it onto the screen (pack = only as big as content)
 myLabel.pack()
 
 # A GUI is always looping in order to notice any interaction, create a loop for our root window
