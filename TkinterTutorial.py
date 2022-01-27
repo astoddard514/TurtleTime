4a84929348f8dc52a922a8d6b4e4d58616e5339a
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-26 22:08:01 -0600

    Add Tkinter Tutorial file and PyQt5 code snippet file

---
+++ TkinterTutorial.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Root widget, your main window
+root = Tk() 
+
+# Creating a label widget
+myLabel = Label(root, text="Hello World!")
+# Shoving it onto the screen
+myLabel.pack()
+
+# A GUI is always looping in order to notice any interaction, create a loop for our root window
+root.mainloop()
