31f90c95f1594656e77c665c5cd7bdca392a3f88
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-01-30 13:11:31 -0600

    Control panel frames and some button work

--- turtletime.py
+++ turtletime.py
@@ -1,18 +1,23 @@
 # Turtle time game for kids
 
+# Learning resources: 
+# https://www.youtube.com/watch?v=yQSEXcf6s2I&list=PLCC34OHNcOtoC6GglhF3ncJ5rLwQrLGnV&index=1
+# https://www.youtube.com/watch?v=lETQ3mvuzkQ
+
 from tkinter import *
 from tkinter import ttk
 import turtle
-import tools 
-
 
 
 # Set up screen
 root = Tk()
 root.title('Turtle Time')
-root.geometry('1500x900')
-# tools.center(root) # center window on screen
-root.config(bg= 'indigo')
+root.geometry('1500x800+100+100')
+root.config(bg= '#340f4f')
+
+
+
+
 
 class Shapes:
 
@@ -53,47 +58,127 @@ class Movements:
 
 
 # Canvas on left
-canvas = Canvas(root, width= 800, height= 800)
+canvas = Canvas(root, width= 800, height= 700)
+canvas.grid(row=0, rowspan= 3, column=0, padx=50, pady=50)
 
-canvas.grid(rowspan=12, column=0, padx=50, pady=50)
 
-# Frames on right
 
 # Create turtle
 tansy= turtle.RawTurtle(canvas)
 tansy.shape("turtle")
 tansy.resizemode("auto")
 tansy.width(5)
-
 tansy.color("#013220")
 tansy.forward(100)
-tansy.right(45)
-tansy.forward(100)
-tansy.right(45)
-tansy.forward(100)
-tansy.right(45)
 
 # Buttons
 
-ft= 'tahoma 10 bold'
+ft= 'Helvetica 15 bold'
 btn_style= ttk.Style()
-btn_style.configure('TButton', background= 'cyan', forground= 'black', font=ft)
+btn_style.configure('TButton', background= 'black', forground= 'black', font=ft)
+
+# Draw a Shape Frame
+draw_a_shape = LabelFrame(root, text="DRAW A SHAPE", padx=50, pady=50, background= '#340f4f') # padding inside of frame
+draw_a_shape.grid(column= 1, row=0,)
+
+triangle_btn= ttk.Button(draw_a_shape, text= 'Triangle', style= 'TButton')
+square_btn= ttk.Button(draw_a_shape, text= 'Square', style= 'TButton')
+circle_btn= ttk.Button(draw_a_shape, text= 'Circle', style= 'TButton')
+ellipse_btn= ttk.Button(draw_a_shape, text= 'Ellipse', style= 'TButton')
+pentagon_btn= ttk.Button(draw_a_shape, text= 'Pentagon', style= 'TButton')
+hexagon_btn= ttk.Button(draw_a_shape, text= 'Hexagon', style= 'TButton')
+septagon_btn= ttk.Button(draw_a_shape, text= 'Septagon', style= 'TButton')
+octagon_btn= ttk.Button(draw_a_shape, text= 'Octagon', style= 'TButton')
+star_btn= ttk.Button(draw_a_shape, text= 'Star', style= 'TButton')
+spirograph_btn= ttk.Button(draw_a_shape, text= 'Spirograph', style= 'TButton')
+cute_btn= ttk.Button(draw_a_shape, text= 'Cute', style= 'TButton')
 
 
-foward_btn= ttk.Button(root, text= 'Forward')
-backward_btn= ttk.Button(root, text= 'Backward')
-right_btn= ttk.Button(root, text= 'Left')
-left_btn= ttk.Button(root, text= 'Right')
+triangle_btn.grid(row= 1, column=1)
+square_btn.grid(row= 2, column=1)
+circle_btn.grid(row= 3, column=1)
+ellipse_btn.grid(row= 4, column=1)
+pentagon_btn.grid(row= 1, column= 2)
+hexagon_btn.grid(row= 2, column= 2)
+septagon_btn.grid(row= 3, column= 2)
+octagon_btn.grid(row= 4, column= 2)
+star_btn.grid(row= 1, column= 3)
+spirograph_btn.grid(row= 2, column= 3)
+cute_btn.grid(row= 3, column= 3)
+
+# Build a Shape Frame
+
+build_a_shape = LabelFrame(root, text="BUILD A SHAPE", padx=50, pady=50, background= '#340f4f') # padding inside of frame
+build_a_shape.grid(column= 1, row=1)
+
+triangle_btn= ttk.Button(build_a_shape, text= 'Triangle', style= 'TButton')
+square_btn= ttk.Button(build_a_shape, text= 'Square', style= 'TButton')
+circle_btn= ttk.Button(build_a_shape, text= 'Circle', style= 'TButton')
+ellipse_btn= ttk.Button(build_a_shape, text= 'Ellipse', style= 'TButton')
+pentagon_btn= ttk.Button(build_a_shape, text= 'Pentagon', style= 'TButton')
+hexagon_btn= ttk.Button(build_a_shape, text= 'Hexagon', style= 'TButton')
+septagon_btn= ttk.Button(build_a_shape, text= 'Septagon', style= 'TButton')
+octagon_btn= ttk.Button(build_a_shape, text= 'Octagon', style= 'TButton')
+star_btn= ttk.Button(build_a_shape, text= 'Star', style= 'TButton')
+spirograph_btn= ttk.Button(build_a_shape, text= 'Spirograph', style= 'TButton')
+cute_btn= ttk.Button(build_a_shape, text= 'Cute', style= 'TButton')
 
 
-foward_btn.grid(row= 1, column=6)
-backward_btn.grid(row=3, column=6)
-right_btn.grid(row=2, column=7)
-left_btn.grid(row=2, column=5)
+triangle_btn.grid(row= 1, column=1)
+square_btn.grid(row= 2, column=1)
+circle_btn.grid(row= 3, column=1)
+ellipse_btn.grid(row= 4, column=1)
+pentagon_btn.grid(row= 1, column= 2)
+hexagon_btn.grid(row= 2, column= 2)
+septagon_btn.grid(row= 3, column= 2)
+octagon_btn.grid(row= 4, column= 2)
+star_btn.grid(row= 1, column= 3)
+spirograph_btn.grid(row= 2, column= 3)
+cute_btn.grid(row= 3, column= 3)
+
+# Controls Frame
+
+controls = LabelFrame(root, text="CONTROLS", padx=50, pady=50, background= '#340f4f') # padding inside of frame
+controls.grid(column= 1, row=2)
+
+colors= ['red', 'purple', 'yellow',
+        'pink', 'blue', 'lightblue', 
+        'brown', 'white', 'orange']
+
+pen_color= ttk.Combobox(controls, values= colors)
+pen_color.config(font= ft, width= 10)
+pen_color.set('Change Color')
+pen_color.grid(row= 1, column= 1)
+pen_color['state']= 'readonly'
+
+penup_btn= ttk.Button(controls, text= 'Pen Up', style= 'TButton')
+pendown_btn= ttk.Button(controls, text= 'Pen Up', style= 'TButton')
+
+foward_btn= ttk.Button(controls, text= 'Forward', style= 'TButton')
+backward_btn= ttk.Button(controls, text= 'Backward', style= 'TButton')
+right_btn= ttk.Button(controls, text= 'Left', style= 'TButton')
+left_btn= ttk.Button(controls, text= 'Right', style= 'TButton')
+
+penup_btn.grid(row= 2, column= 1)
+pendown_btn.grid(row= 3, column= 1)
+foward_btn.grid(row= 1, column=3)
+backward_btn.grid(row= 3, column=3)
+left_btn.grid(row= 2, column=2)
+right_btn.grid(row= 2, column=4)
+
+
+
+
+
+
 
 # Labels
 
 # Inputs
+
+# Colors
+
+
 
 
 
